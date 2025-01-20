733aac6a8f7d8073527cc6257ecbb8060820cfbc
AnasAhmed001 <hafizanasahmed8@gmail.com>
2025-01-20 23:13:09 +0500

    frontend started

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
 import express from "express";
+import { chats } from "./data/data.js";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/api/chats", (req, res) => {
+    res.json(chats);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
